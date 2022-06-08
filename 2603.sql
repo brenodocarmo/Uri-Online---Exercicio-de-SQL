ff8af2b05279b0cef694e2875812e963efd02592
Breno do Carmo Sousa <brenocarmosousa@gmail.com>
2022-06-08 10:54:54 -0300

    2603 - Endereços dos Clientes

---
+++ 2603.sql
@@ -0,0 +1,8 @@
+-- __Titulo__ 2603 - Endereços dos Clientes
+-- __Autor__: Breno do Carmo
+-- __Data__: 07/06/2022
+-- __Versio__: 1.0.0
+
+SELECT name, street
+FROM customers
+WHERE city = 'Porto Alegre';
